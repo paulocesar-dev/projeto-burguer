95818f844fb5a181be568387ece22dfa06834f05
paulocesar34 <santospcesar38@gmail.com>
2024-06-18 12:25:36 -0300

    Meu primeiro commit

---
+++ projeto-burger-/products.js
@@ -0,0 +1,8 @@
+const menuOptions = [
+    { name: 'X-Salada', price: 30, vegan: false, src: './img/x-salada.jpeg' },
+    { name: 'X-Bacon', price: 34, vegan: false, src: './img/x-bacon.png' },
+    { name: 'X-Bacon Egg', price: 39, vegan: false, src: './img/x-bacon-egg.png' },
+    { name: 'X-Vegan', price: 45, vegan: true, src: './img/x-vegan.png' },
+    { name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.png' },
+    { name: 'Big Vegano', price: 55, vegan: true, src: './img/big-vegano.png' },
+    ]
